36d1412b1ef191bd95c49c39c78bf14ec1bfc263
priyanshusuman9 <priyanshusuman947@gmail.com>
2022-12-26 07:48:39 +0530

    Commit

---
+++ src/com/bridgelabz/NewWord.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+    public class HashTableMain {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome To HashTable Program");
+            MyHashTable<String, Integer> hashTable = new MyHashTable();
+            String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            // Storing given paragraph in an array.
+            // Converting the para words to lower case and removing spaces.
+
+            String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+            // Iterating over the array.
+            for (String word : sentenceArray) {
+                Integer value = hashTable.get(word);
+
+                if( value == null)
+                    value = 1;
+                else
+                    value = value + 1;
+                hashTable.add(word , value);
+            }
+            System.out.println(hashTable);
+        }
+
+    }
+
---
+++ src/com/bridgelabz/Remove.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+
+    public class HashTableMain {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome To HashTable Program");
+            MyHashTable<String, Integer> hashTable = new MyHashTable();
+            String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            // Storing given paragraph in an array.
+            // Converting the para words to lower case and removing spaces.
+
+            String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+            // Iterating over the array.
+            for (String word : sentenceArray) {
+                Integer value = hashTable.get(word);
+
+                if( value == null) // //frequency of the word StayWith 1
+                    value = 1;
+                else
+                    value = value + 1; //frequency of the word increases by 1
+                hashTable.add(word , value);
+                System.out.println("Value = "+value);
+            }
+            System.out.println(hashTable);
+
+            //Remove "avoidable" from the hashtable
+            hashTable.remove("avoidable");
+
+            System.out.println(hashTable);
+        }
+    }
+
